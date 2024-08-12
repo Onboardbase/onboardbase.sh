3b766fbd85995be5c2f6b43bf7fef017ad5b61d2
Aleem Isiaka <aleemisiaka@gmail.com>
2024-08-12 12:11:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+## Onboardbase install shell script
+
+The motive is to ease the setup time to getting onboardbase running on an uninstalled machine
+
+### Installation
+```bash
+sudo apt-get install curl
+
+curl -Ss github.com/Onboardbase/onboardbase.sh/main/install.sh | bash -
+```
+
+#### Supported Environment variables
+The script supports these environment variables:
+- ONBOARDBASE_TOKEN: This automatically authenticates the CLI globally, and gives a head start to pulling secrets from onboardbase
+- ONBOARDBASE_API_HOST: If the CLI is to connect with a self-hosted version, this flag can point to where the API is running
+- ONBOARDBASE_DASHBOARD_HOST: If the CLI is to connect with a self-hosted version, this flag can point to where the FRONTEND is running
+- Both ONBOARDBASE_PROJECT and ONBOARDBASE_ENVIRONMENT: This creates a .onboardbase.yaml file in the current directory that this script runs from to store an onboardbase project config file that points to both the project and environment. such that after running the install.sh script, onboardbase secrets --env should list all the secrets in the project ONBOARDBASE_PROJECT and envirnment ONBOARDBASE_ENVIRONMENT
+
+### How it works
+The script installs nodejs if not installed via nvm, before using npm to install onboardbase.
+
+### Supported OS
+This script should work on:
+- Linux
+
+If you need any support for other OSs, please create an issue.
---
+++ install.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+get_package_manager(){
+  if command -v apt >/dev/null 2>&1; then
+    echo "apt"
+  elif command -v yum >/dev/null 2>&1; then
+    echo "yum"
+  elif command -v dnf >/dev/null 2>&1; then
+    echo "dnf"
+  elif command -v pacman >/dev/null 2>&1; then
+    echo "pacman"
+  else
+    echo "unknown"
+  fi
+}
+
+install_deps_with_package_manager() {
+  packages="curl"
+  package_manager=$1
+
+  cmd="$package_manager install $packages -y"
+  if [ "$package_manager" = "pacman" ];
+  then
+	cmd="$package_manager -S $packages"
+  fi
+  log "Installing package dependecies with the command: \"$cmd\""
+  eval "$cmd >/dev/null"
+}
+
+install_deps(){
+  package_manager=$(get_package_manager)
+  case "$package_manager" in
+    "yum" | "dnf" | "apt" | "pacman")
+      install_deps_with_package_manager $package_manager
+      ;;
+    *)
+      log "Unsuppoerted package manager. Please install Node.js manually"
+      ;;
+   esac
+}
+
+
+install_nodejs(){
+  install_deps # installs the dependencies that is required for the nodejs setup
+  curl -sS https://gist.githubusercontent.com/chrisidakwo/5f228cb0883efdcfae1a880f80b9744b/raw/739937195fe450e80083af057d67b8ebfd1af570/install-node.sh | sh -
+}
+
+
+install_obb_cli(){
+  log "Installing OBB CLI via npm"
+  npm install -g @onboardbase/cli >/dev/null 2>&1
+  log "OBB CLI installed successfully"
+}
+
+
+is_cmd_installed(){
+ command -v "$1" >/dev/null 2>&1;
+}
+
+log(){
+    echo ">>>  $1"
+}
+
+configure_obb_cli_token(){
+  log "Configuring onboardbase token"
+  onboardbase config:set --token=$ONBOARDBASE_TOKEN --scope=/
+}
+
+configure_obb_cli_api_host(){
+  log "Configuring onboardbase api host"
+  onboardbase config:set --api-host=$ONBOARDBASE_API_HOST --scope=/
+}
+
+
+configure_obb_cli_dashboard_host(){
+  log "Configuring onboardbase dashboard host"
+  onboardbase config:set --dashboard-host=$DASHBOARD_HOST --scope=/
+}
+
+create_yml_file(){
+yml=$(cat <<EOF
+api_key:
+setup:
+  project: $1
+  environment: $2
+secrets:
+  local:
+EOF
+)
+file_path="$(pwd)/.onboardbase.yaml"
+echo "$yml" > $file_path
+log "OBB Setup YAML file written to $file_path"
+}
+
+# =======================
+# Run
+# =======================
+
+if is_cmd_installed onboardbase; 
+then
+  log "onboardbase$(onboardbase -v) is already installed";
+  exit 0;
+fi
+
+# ensures that nodejs is intalled
+if !is_cmd_installed node;
+then
+  install_nodejs;
+fi
+
+install_obb_cli
+
+if [ -n "$ONBOARDBASE_TOKEN" ];
+then
+  configure_obb_cli_token;
+fi
+
+if [ -n "$ONBOARDBASE_PROJECT" && -n "$ONBOARDBASE_ENVIRONMENT"];
+then
+  create_yml_file $ONBOARDBASE_PROJECT $ONBOARDBASE_ENVIRONMENT;
+fi
+
+if [ -n "$ONBOARDBASE_API_HOST" ];
+then
+  configure_obb_cli_api_host;
+fi
