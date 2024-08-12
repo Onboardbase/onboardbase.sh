f8021aa8f1657e5819266fcd7e87dfe4c21d45ab
Aleem Isiaka <aleemisiaka@gmail.com>
2024-08-12 13:00:17 +0100

    setup install nodejs script

---
+++ install_nodejs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+# Install node and npm via nvm - https://github.com/creationix/nvm
+
+# Define versions
+INSTALL_NODE_VER=10
+INSTALL_NVM_VER=0.33.11
+INSTALL_YARN_VER=1.7.0
+
+echo "==> Installing node version manager (NVM). Version $INSTALL_NVM_VER"
+# Removed if already installed
+rm -rf ~/.nvm
+# Unset exported variable
+export NVM_DIR=
+
+# Install nvm 
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$INSTALL_NVM_VER/install.sh | bash
+# Make nvm command available to terminal
+source ~/.nvm/nvm.sh
+
+echo "==> Installing node js version $INSTALL_NODE_VER"
+nvm install $INSTALL_NODE_VER
+
+echo "==> Make this version system default"
+nvm alias default $INSTALL_NODE_VER
+nvm use default
+
+
+echo "==> Checking for versions"
+nvm --version
+node --version
+npm --version
+
+nvm cache clear
